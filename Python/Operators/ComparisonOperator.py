c7534a7cef4d1b280cc77c8837de9f1fb081b6b6
MrAyushBajpai <ayushbajpai1711@gmail.com>
2024-07-24 13:52:44 +0530

    Added Comparsion Operator Example in Python

---
+++ Python/Operators/ComparisonOperator.py
@@ -0,0 +1,34 @@
+#Comparison Operator
+
+num1 = 10
+num2 = 5
+
+# Equal to
+equal = num1 == num2
+# Checks if 10 is equal to 5, results in False
+print("Equal to:", equal)
+
+# Not equal to
+not_equal = num1 != num2
+# Checks if 10 is not equal to 5, results in True
+print("Not equal to:", not_equal)
+
+# Greater than
+greater_than = num1 > num2
+# Checks if 10 is greater than 5, results in True
+print("Greater than:", greater_than)
+
+# Less than
+less_than = num1 < num2
+# Checks if 10 is less than 5, results in False
+print("Less than:", less_than)
+
+# Greater than or equal to
+greater_than_or_equal = num1 >= num2
+# Checks if 10 is greater than or equal to 5, results in True
+print("Greater than or equal to:", greater_than_or_equal)
+
+# Less than or equal to
+less_than_or_equal = num1 <= num2
+# Checks if 10 is less than or equal to 5, results in False
+print("Less than or equal to:", less_than_or_equal)
