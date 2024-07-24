b74211652bbd5ce8fc7cbcc8b5315503f40bb0b4
MrAyushBajpai <ayushbajpai1711@gmail.com>
2024-07-24 13:51:08 +0530

    Added Arithmetic Operator Example in Python

---
+++ Python/Operators/ArtihmeticOperator.py
@@ -0,0 +1,39 @@
+#Arithmetic Operator
+
+num1 = 10
+num2 = 5
+
+# Addition
+addition = num1 + num2
+# Adding 10 and 5 results in 15
+print("Addition:", addition)
+
+# Subtraction
+subtraction = num1 - num2
+# Subtracting 5 from 10 results in 5
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = num1 * num2
+# Multiplying 10 by 5 results in 50
+print("Multiplication:", multiplication)
+
+# Division
+division = num1 / num2
+# Dividing 10 by 5 results in 2.0
+print("Division:", division)
+
+# Floor Division
+floor_division = num1 // num2
+# Floor dividing 10 by 5 results in 2 (discards the fractional part)
+print("Floor Division:", floor_division)
+
+# Modulus
+modulus = num1 % num2
+# The remainder when 10 is divided by 5 is 0
+print("Modulus:", modulus)
+
+# Exponentiation
+exponentiation = num1 ** num2
+# Raising 10 to the power of 5 results in 100000
+print("Exponentiation:", exponentiation)
