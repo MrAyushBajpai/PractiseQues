368830b77a0563f7e667ee4c9a30c89cafcdfd99
MrAyushBajpai <ayushbajpai1711@gmail.com>
2024-10-29 21:39:48 +0530

    Solved Leetcode Problem 69

---
+++ C++/DSA/Problems/Leetcode/LC69.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/sqrtx/
+
+#include <iostream>
+using namespace std;
+
+int mySqrtBinary(int x) {
+    int min = 1, max = x/2;
+    while(min <= max){
+        long mid = min+(max-min)/2;
+        if (mid * mid == x){
+            return mid;
+        }
+        else if (mid * mid < x){
+            min = mid + 1;
+        }
+        else{
+            max = mid - 1;
+        }
+    }
+    return max;
+}
+
+int main(){
+    cout << mySqrt(18);
+}
