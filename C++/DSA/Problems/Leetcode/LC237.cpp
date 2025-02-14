5bc31a0c55d49e287c4439acc6712d0752f41d39
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-02-14 11:22:55 +0530

    Solved Leetcode Problem 237 in C++

---
+++ C++/DSA/Problems/Leetcode/LC237.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+void deleteNode(ListNode* node) {
+    node->val = node->next->val;
+    ListNode* temp = node->next;
+    node->next = node->next->next;
+    delete temp;
+}
+
+int main(){
+    ListNode* head = new ListNode(1);
+    ListNode* first = new ListNode(2);
+    ListNode* second = new ListNode(3);
+    ListNode* third = new ListNode(4);
+    ListNode* fourth = new ListNode(5);
+
+    head->next = first;
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = third;
+
+    deleteNode(second);
+
+}
