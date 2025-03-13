7adf76b15fa1481f5da29cfed3de328869b5a9ae
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-03-13 20:32:38 +0530

    Added Assignment Operators Example in Dart

---
+++ Dart/BasicExamples/10. AssignmentOperator.dart
@@ -0,0 +1,18 @@
+void main(){
+    int num = 10;
+
+    num += 5;
+    print("Addition Assignment: $num");
+
+    num -= 3;
+    print("Subtraction Assignment: $num");
+
+    num *= 2;
+    print("Multiplication Assignment: $num");
+
+    num ~/= 4;
+    print("Integer Division Assignment: $num");
+
+    num %= 5;
+    print("Modulus Assignment: $num"); 
+}
