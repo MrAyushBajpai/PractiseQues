6068241b9ed7f9aabaa5237728379f22ac3270a6
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-03-13 19:09:21 +0530

    Added Data Types Example in Dart

---
+++ Dart/BasicExamples/2. DataTypes.dart
@@ -0,0 +1,35 @@
+void main(){
+    // Integer
+    int age = 25;
+
+    // Double
+    double height = 5.7;
+
+    // String
+    String name = 'Ayush';
+
+    // Boolean
+    bool isStudent = true;
+
+    // List (array)
+    List<String> fruits = ['Apple', 'Banana', 'Cherry'];
+
+    // Map
+    Map<String, int> scores = {'Akhil': 90, 'Bobby':50};
+
+    // Dyanamic (Hold any data type)
+    dynamic variable = "Hello";
+    variable = 123;
+
+    // Var (Automatic Type Inference)
+    var weight = 62;
+
+    print('Age: $age');
+    print('Height: $height');
+    print('Name: $name');
+    print('Is Student?: $isStudent');
+    print('Fruits: $fruits');
+    print('Scores: $scores');
+    print('Dynamic Variable: $variable');
+    print('var Variable: $weight');
+}
