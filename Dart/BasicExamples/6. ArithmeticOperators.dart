cf12d08687f7baab43e2b34c55b8cc1ccdabadd6
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-03-13 20:06:03 +0530

    Added Arithmetic Operator Example in Dart

---
+++ Dart/BasicExamples/6. ArithmeticOperators.dart
@@ -0,0 +1,10 @@
+void main(){
+    int a = 10, b = 3;
+
+    print("Addition: ${a+b}");
+    print("Subtraction: ${a-b}");
+    print("Multiplication: ${a*b}");
+    print("Division: ${a/b}");
+    print("Integer Division: ${a~/b}");
+    print("Modulus: ${a%b}");
+}
