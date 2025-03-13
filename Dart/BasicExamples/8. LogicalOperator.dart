cae965eaa1a7240a15b3df12c1802170a64181ca
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-03-13 20:17:33 +0530

    Added Logical Operators Example in Dart

---
+++ Dart/BasicExamples/8. LogicalOperator.dart
@@ -0,0 +1,7 @@
+void main(){
+    bool isSunny = true, isWeekend = false;
+
+    print("Logical AND: ${isSunny && isWeekend}");
+    print("Logical OR: ${isSunny || isWeekend}");
+    print("Logical NOT: ${!isSunny}");
+}
