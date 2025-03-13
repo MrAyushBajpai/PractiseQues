34d0c1b68346c31d2ac51dfe99c778e828ffd957
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-03-13 20:14:59 +0530

    Added Relational Operators Example in Dart

---
+++ Dart/BasicExamples/7. RelationalOperator.dart
@@ -0,0 +1,10 @@
+void main(){
+    int x = 5, y = 10;
+
+    print("Equality: ${x == y}");
+    print("Unequality: ${x != y}");
+    print("Greater Than: ${x > y}");
+    print("Less Than: ${x < y}");
+    print("Greater Than or Equal: ${x >= y}");
+    print("Less Than or Equal: ${x <= y}");
+}
