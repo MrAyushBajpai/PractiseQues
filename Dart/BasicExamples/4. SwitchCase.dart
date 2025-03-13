498e61916433cc66d023bbc4170b560022ca320b
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-03-13 19:24:16 +0530

    Added Switch Case Example in Dart

---
+++ Dart/BasicExamples/4. SwitchCase.dart
@@ -0,0 +1,20 @@
+void main(){
+    String grade = 'B';
+
+    switch(grade){
+        case 'A':
+            print('Excellent!');
+            break;
+        case 'B':
+            print('Good Job!');
+            break;
+        case 'C':
+            print('You can do better');
+            break;
+        case 'D':
+            print('Try Harder Next Time');
+            break;
+        default:
+            print('Invalid Grade'); 
+    }
+}
