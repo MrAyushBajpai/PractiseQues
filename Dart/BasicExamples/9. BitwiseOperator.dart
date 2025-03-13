e146d4e6fa5b963414c58cacde20bfd4b244d9d0
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-03-13 20:27:05 +0530

    Added Bitwise Operators Example in Dart

---
+++ Dart/BasicExamples/9. BitwiseOperator.dart
@@ -0,0 +1,11 @@
+void main(){
+    int a = 5; // Binary: 0101
+    int b = 3; // Binary: 0011
+
+    print("Bitwise AND: ${a & b}");
+    print("Bitwise OR: ${a | b}");
+    print("Bitwise XOR: ${a ^ b}");
+    print("Bitwise NOT: ${~a}");
+    print("Bitwise Left Shift: ${a << 1} ");
+    print("Bitwise Right Shift: ${a >> 1}");
+}
