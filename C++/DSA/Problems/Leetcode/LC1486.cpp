b878227eca5086c7198f2dcd051cd6513861d5ca
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-09-28 09:14:41 +0530

    Solved Leetcode Problem 1486 in C++

---
+++ C++/DSA/Problems/Leetcode/LC1486.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int xorOperation(int n, int start) {
+    int res = 0;
+    for(int i = 0; i < n; i++){
+        res ^= start + 2 * i;
+    }
+
+    return res;
+}
+
+int main(){
+    cout << xorOperation(5, 0);
+}
